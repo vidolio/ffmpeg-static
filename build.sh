386808d714dee4669f64ea9b99b766c8bb07582c
vidolio <agale@exocurve.com>
2012-04-15 21:50:01 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -30,70 +30,70 @@ cd $BUILD_DIR
 ../fetchurl "http://www.ffmpeg.org/releases/ffmpeg-0.10.2.tar.gz"
 
 echo "*** Building yasm ***"
-cd "$BUILD_DIR/yasm-1.0.1"
+cd "$BUILD_DIR/yasm-1.2.0"
 ./configure --prefix=$TARGET_DIR
-make -j 4 && make install
+make -j 2 && make install
 
 echo "*** Building zlib ***"
 cd "$BUILD_DIR/zlib-1.2.6"
 ./configure --prefix=$TARGET_DIR
-make -j 4 && make install
+make -j 2 && make install
 
 echo "*** Building bzip2 ***"
-cd "$BUILD_DIR/bzip2-1.0.5"
+cd "$BUILD_DIR/bzip2-1.0.6"
 make
 make install PREFIX=$TARGET_DIR
 
 echo "*** Building libpng ***"
-cd "$BUILD_DIR/libpng-1.2.47"
+cd "$BUILD_DIR/libpng-1.5.9"
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
-make -j 4 && make install
+make -j 2 && make install
 
 # Ogg before vorbis
 echo "*** Building libogg ***"
-cd "$BUILD_DIR/libogg-1.2.0"
+cd "$BUILD_DIR/libogg-1.3.0"
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
-make -j 4 && make install
+make -j 2 && make install
 
 # Vorbis before theora
 echo "*** Building libvorbis ***"
-cd "$BUILD_DIR/libvorbis-1.3.1"
+cd "$BUILD_DIR/libvorbis-1.3.3"
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
-make -j 4 && make install
+make -j 2 && make install
 
 echo "*** Building libtheora ***"
 cd "$BUILD_DIR/libtheora-1.1.1"
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
-make -j 4 && make install
+make -j 2 && make install
 
 echo "*** Building livpx ***"
-cd "$BUILD_DIR/libvpx-0.9.1"
+cd "$BUILD_DIR/libvpx-v1.0.0"
 ./configure --prefix=$TARGET_DIR --disable-shared
-make -j 4 && make install
+make -j 2 && make install
 
 echo "*** Building faac ***"
 cd "$BUILD_DIR/faac-1.28"
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
 # FIXME: gcc incompatibility, does not work with log()
 sed -i -e "s|^char \*strcasestr.*|//\0|" common/mp4v2/mpeg4ip.h
-make -j 4 && make install
+make -j 2 && make install
 
 echo "*** Building x264 ***"
-cd "$BUILD_DIR/x264-snapshot-20100620-2245"
+cd "$BUILD_DIR/x264-snapshot-20120408-2245-stable"
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
-make -j 4 && make install
+make -j 2 && make install
 
 
 echo "*** Building xvidcore ***"
 cd "$BUILD_DIR/xvidcore/build/generic"
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
-make -j 4 && make install
+make -j 2 && make install
 #rm $TARGET_DIR/lib/libxvidcore.so.*
 
 echo "*** Building lame ***"
-cd "$BUILD_DIR/lame-3.98.4"
+cd "$BUILD_DIR/lame-3.99.5"
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
-make -j 4 && make install
+make -j 2 && make install
 
 # FIXME: only OS-sepcific
 rm -f "$TARGET_DIR/lib/*.dylib"
@@ -101,8 +101,7 @@ rm -f "$TARGET_DIR/lib/*.so"
 
 # FFMpeg
 echo "*** Building FFmpeg ***"
-cd "$BUILD_DIR/ffmpeg-0.6.1"
+cd "$BUILD_DIR/ffmpeg-0.10.2"
 patch -p1 <../../ffmpeg_config.patch
 CFLAGS="-I$TARGET_DIR/include" LDFLAGS="-L$TARGET_DIR/lib -lm" ./configure --prefix=${OUTPUT_DIR:-$TARGET_DIR} --extra-version=static --disable-debug --disable-shared --enable-static --extra-cflags=--static --disable-ffplay --disable-ffserver --disable-doc --enable-gpl --enable-pthreads --enable-postproc --enable-gray --enable-runtime-cpudetect --enable-libfaac --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-bzlib --enable-zlib --enable-nonfree --enable-version3 --enable-libvpx --disable-devices
-make -j 4 && make install
-
+make -j 2 && make install
